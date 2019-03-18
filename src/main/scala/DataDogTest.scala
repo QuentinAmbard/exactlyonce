bc9cb478a2744e68122954a310873492e26cb900
quentin <quentin.ambard@datastax.com>
2019-03-18 17:58:40 +0100

    test structured

--- src/main/scala/DataDogTest.scala
+++ src/main/scala/DataDogTest.scala
@@ -14,7 +14,7 @@ object DataDogTest extends App {
   val metrics = new MetricRegistry();
   val requests = metrics.meter("test");
   val expansions = util.EnumSet.of(COUNT, RATE_1_MINUTE, RATE_15_MINUTE, MEDIAN, P95, P99)
-  val httpTransport = new HttpTransport.Builder().withApiKey("257990b896642192db8fd0a8d31e1993").withProxy("",1).build()
+  val httpTransport = new HttpTransport.Builder().withApiKey("xxxxxx").withProxy("",1).build()
   val reporter = DatadogReporter.forRegistry(metrics)
     .withTransport(httpTransport)
     .withExpansions(expansions)
